e54feeea5e148d4db4a2f6c8d95f35938c60971f
yugendragitz <yugendrastcksdmt@gmail.com>
2025-11-29 12:09:34 +0530

    Fix axios env variable

--- src/lib/axiosinstance.js
+++ src/lib/axiosinstance.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 const axiosInstance = axios.create({
-  baseURL: process.env.BACKEND_URL,
- 
+  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
 });
 export default axiosInstance;
